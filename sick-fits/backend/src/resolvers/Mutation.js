9ea96a84451a54c3531c0a3034d0c3eca7dad7c8
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-08-25 17:35:39 +0530

    making first query and mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    // never store in memory at global scope, because every time server restart, you'll loose everything
+    global.dogs = global.dogs || [];
+    // create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    // never store in memory at global scope, because every time server restart, you'll loose everything
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
